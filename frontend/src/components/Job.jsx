2898ea2007e2cdd91ba53e9af5eb324cb9dc1d07
mars050504 <153522723+mars050504@users.noreply.github.com>
2025-06-02 19:29:42 +0700

    Update Job.jsx

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,67 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Badge } from './ui/badge'
+import React from 'react'
 import { Button } from './ui/button'
-import { setSingleJob } from '../redux/jobSlice';
-import { JOB_API_END_POINT, APPLICATION_API_END_POINT } from '@/utils/constant';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { toast } from 'sonner';
-import { Avatar, AvatarImage } from './ui/avatar';
+import { Bookmark } from 'lucide-react'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Badge } from './ui/badge'
+import { useNavigate } from 'react-router-dom'
 
-
-const Job = ({ job }) => {
-    const { singleJob } = useSelector(store => store.job);
-    const { user } = useSelector(store => store.auth);
-    const isIntiallyApplied = singleJob?.applications?.some(application => application.applicant === user?._id) || false;
-    const [isApplied, setIsApplied] = useState(isIntiallyApplied);
-
-    const params = useParams();
-    const jobId = params.id;
-    const dispatch = useDispatch();
-
-    const applyJobHandler = async () => {
-        try {
-            const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, { withCredentials: true });
-            console.log(res.data);
-            if (res.data.success) {
-                setIsApplied(true); //update the local state
-                const updateSingleJob = { ...singleJob, applications: [...singleJob.applications, { applicant: user?._id }] }
-                dispatch(setSingleJob(updateSingleJob)); //helps to real time UI update
-                toast.success(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        }
-    }
-
-    useEffect(() => {
-        const fetchSingleJob = async () => {
-            try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, { withCredentials: true });
-                if (res.data.success) {
-                    dispatch(setSingleJob(res.data.job));
-                    setIsApplied(res.data.job.applications.some(application => application.applicant === user?._id)) //ensure the state is in sync with fetch data
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchSingleJob();
-    }, [jobId, dispatch, user?._id]);
+const Job = ({job}) => {
+    const navigate = useNavigate();
     // const jobId = "ashdahsahwd";
 
     const daysAgoFunction = (mongodbTime) => {
         const createdAt = new Date(mongodbTime);
         const currentTime = new Date();
         const timeDifference = currentTime - createdAt;
-        return Math.floor(timeDifference / (1000 * 24 * 60 * 60));
+        return Math.floor(timeDifference/(1000*24*60*60));
     }
     return (
         <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
             <div className='flex items-center justify-between'>
-                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) == 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
+                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt)==0? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
+                <Button variant="outline" className='rounded-full' size="icon"><Bookmark /></Button>
             </div>
             <div className='flex items-center gap-2 my-2'>
                 <Button className="p-6" variant="rounded-full" size="icon">
@@ -71,7 +29,7 @@ const Job = ({ job }) => {
                 </Button>
                 <div>
                     <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
-                    <p className='text-sm text-gray-500'>Indonesia</p>
+                    <p className='text-sm text-gray-500'>{job?.company.location}</p>
                 </div>
             </div>
             <div>
@@ -84,13 +42,8 @@ const Job = ({ job }) => {
                 <Badge className={'text-[#7209B7] font-bold'} variant="ghost">{job?.salary}</Badge>
             </div>
             <div className='flex items-center gap-4 mt-4'>
-                <Button onClick={() => navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
-                <Button
-                    onClick={isApplied ? null : applyJobHandler}
-                    disabled={isApplied}
-                    className={`rounded-lg ${isApplied ? 'bg-gray-600 cursor-not-allowed' : 'bg-[#7209B7] hover:bg-[5f3ad] cursor-pointer'}`}>
-                    {isApplied ? 'Already Applied' : 'Apply Now'}
-                </Button>
+                <Button onClick={() => navigate (`/description/${job?._id}`)} variant="outline">Details</Button>
+                <Button className="bg-[#7209b7]">Save For Later</Button>
             </div>
 
         </div>
